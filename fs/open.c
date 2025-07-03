94a5d4beb67496be627478f610f591fb4658cffd
ebeth03 <ebeth03@github.com>
2025-07-03 20:49:21 +0700

    implement KernelSU-Next v1.5.5 to v1.5.7

--- fs/open.c
+++ fs/open.c
@@ -362,6 +362,11 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
 	return error;
 }
 
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_WITH_KPROBES)
+extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
+			                    int *flags);
+#endif
+
 /*
  * access() needs to use the real uid/gid, not the effective uid/gid.
  * We do this by temporarily clearing all FS-related capabilities and
@@ -377,7 +382,7 @@ SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
 	int res;
 	unsigned int lookup_flags = LOOKUP_FOLLOW;
 
-#ifdef CONFIG_KSU
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_WITH_KPROBES)
 	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
 #endif		
 
